f9ad5ecffb30caccb7e6ce24d2ed0397d54d7090
faturachmanyusup <faturachmany@gmail.com>
2021-11-20 02:32:24 +0700

    fix logo missing

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ module.exports = withPWA({
   pwa: {
     disable: process.env.PWA === 'DISABLE',
     dest: 'public',
-    runtimeCaching: []
+    register: true,
+    skipWaiting: true
   }
 })
--- pages/_document.js
+++ pages/_document.js
@@ -9,10 +9,7 @@ class MyDocument extends Document {
           <meta name='application-name' content='PB-Learning' />
           <meta name='description' content='Belajar kapanpun dan di manapun dengan PB Learning' />
           <meta name='theme-color' content='#FF4B2B' />
-
-          <link rel='icon' sizes='192x192' href='/assets/icons/e-classes-logo.png' />
-          <link rel='apple-touch-icon' href='/assets/icons/e-classes-logo.png' />
-
+          <link rel='apple-touch-icon' href='/assets/e-classes-logo.png' />
           <link rel='manifest' href='/manifest.json' />
         </Head>
         <body>
